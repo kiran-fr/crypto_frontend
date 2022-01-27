55d9065cd130cd152bb7f58166aa6f8996ef422d
kiran_fr <kiranreddy8244@gmail.com>
2022-01-27 23:02:43 +0530

    added api link

--- src/constants/urlConst.js
+++ src/constants/urlConst.js
@@ -1,2 +1,2 @@
-export const url = 'http://localhost:5000';
+export const url = 'https://crypto-web-apps.herokuapp.com';
 export const cryptUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=316c2391-bf23-4c2c-a357-130e23852778`
